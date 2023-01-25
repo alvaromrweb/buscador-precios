f021d59e8977432842a44277e4e2f883b9cdca12
alvaromrweb <alvaromrweb@gmail.com>
2023-01-25 14:43:50 +0100

    Adding webicon to skeleton

--- components/SkeletonResult.js
+++ components/SkeletonResult.js
@@ -8,11 +8,15 @@ export default function SkeletonResult() {
                 <div className="rounded bg-slate-600 h-24 w-24"></div>
             </div>
             <div className='relative flex-1 space-y-6 py-1'>
+                <div className="h-2 bg-slate-600 rounded w-9/12"></div>
                 <div className="h-2 bg-slate-600 rounded w-10/12"></div>
-                <div className="h-2 bg-slate-600 rounded w-11/12"></div>
+                <div className="h-2 bg-slate-600 rounded w-32"></div>
             </div>
             <div className='absolute right-2 top-1'>
-                <div className="h-2 bg-slate-600 rounded w-32"></div>
+                <div className='flex gap-2 items-center'>
+                    <div className="rounded-full bg-slate-600 h-5 w-5"></div>
+                    <div className="h-2 bg-slate-600 rounded w-20 hidden md:block"></div>
+                </div>
             </div>
             
         </div>

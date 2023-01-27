59fda6745ab6ac3297dbfa21d4e15c4759944720
alvaromrweb <alvaromrweb@gmail.com>
2023-01-27 14:20:23 +0100

    Add responsive design to skeleton loading

--- components/SkeletonResult.js
+++ components/SkeletonResult.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function SkeletonResult() {
   return (
     <div className='w-full rounded-lg bg-slate-700 shadow-lg p-4 mx-auto animate-in fade-in duration-500'>
-        <div className="animate-pulse flex space-x-4 relative">
-            <div className='mb-4 md:mb-0'>
-                <div className="rounded bg-slate-600 h-24 w-24"></div>
+        <div className="animate-pulse flex flex-col md:flex-row md:space-x-4 relative">
+            <div className='mb-4 md:mb-0 text-center md:text-left'>
+                <div className="rounded bg-slate-600 h-24 w-24 mx-auto md:mx-0"></div>
             </div>
             <div className='relative flex-1 space-y-6 py-1'>
-                <div className="h-2 bg-slate-600 rounded w-9/12"></div>
-                <div className="h-2 bg-slate-600 rounded w-10/12"></div>
-                <div className="h-2 bg-slate-600 rounded w-32"></div>
+                <div className="h-2 bg-slate-600 rounded w-9/12 mx-auto md:mx-0"></div>
+                <div className="h-2 bg-slate-600 rounded w-10/12 mx-auto md:mx-0"></div>
+                <div className="h-2 bg-slate-600 rounded w-32 mx-auto md:mx-0"></div>
             </div>
             <div className='absolute right-2 top-1'>
                 <div className='flex gap-2 items-center'>

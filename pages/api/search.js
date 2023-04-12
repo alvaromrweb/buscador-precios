d8c807d024df81e2e501c101988c996e780fde12
alvaromrweb <alvaromrweb@gmail.com>
2023-04-12 14:14:41 +0200

    Fix for vercel deploy with puppeteer

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer'
+import chromium from 'chrome-aws-lambda';
 import { KnownDevices } from 'puppeteer'
 import fs from 'fs'
 
@@ -10,8 +11,14 @@ export default async function handler(req, res) {
   // Cargar version movil para sacar el icono de la web
   const Iphone = KnownDevices['iPhone 8']
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  const browser = await chromium.puppeteer.launch({
+    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    executablePath: await chromium.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  });
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
   await page.emulate(Iphone);
 
   await page.goto(`https://www.google.es/search?q=${search}`);
@@ -25,7 +32,7 @@ export default async function handler(req, res) {
     items.forEach(item => { 
       const title = item.querySelector("div[role='heading'][aria-level='3']")?.innerText
       const link = item.querySelector("a")?.href
-      const description = item.querySelector("[data-content-feature='1'] > div")?.innerText
+      const description = item.querySelector(".MUxGbd.yDYNvb")?.innerText
       const web = item.querySelector(".Aozhyc")?.innerText
       const webImg = item.querySelector("img")?.src
 
@@ -37,11 +44,11 @@ export default async function handler(req, res) {
         price = price ? price[0] : null
       }
 
-      let img = item.querySelector("div[data-sokoban-feature='Vjbam'] img")?.src
+      let img = item.querySelector(".LicuJb.uhHOwf img")?.src
 
       // Si la imagen tiene esa cadena, es que no ha cargado aun
       if(img == "data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==") {
-        const imgId = item.querySelector("div[data-sokoban-feature='Vjbam'] img")?.id
+        const imgId = item.querySelector(".LicuJb.uhHOwf img")?.id
         // Aqui estoy encontrando en un script mediante el id de la imagen, la url de la imagen que carga mas tarde
         const imgUrl = document.documentElement.outerHTML.split(`${imgId}\"\:\"`)[1].split('\"')[0]  
         img = JSON.parse(`"${imgUrl}"`)
